0f27945bae993c2372b9c4be535f75ab84036fe9
Han Wang <92250561+wanghan9603@users.noreply.github.com>
2023-08-03 23:41:17 +0800

    Add files via upload

---
+++ ClearAll.m
@@ -0,0 +1,3 @@
+function ClearAll()
+clear functions
+end
---
+++ sum_diff.m
@@ -0,0 +1,6 @@
+function [sum, diff] = sum_diff(x1, x2)
+    sum = x1 + x2;
+    diff = x1 - x2;
+%     sum = x1 * x2;
+%     diff = x1 / x2;
+end
